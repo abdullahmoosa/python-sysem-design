b86b3e699cf69af07d457ed97064afc9f0ef1c35
Abdullah Muhammad Moosa <abdullahmoosa128@gmail.com>
2024-04-05 16:21:09 +0600

    Fix: correct sql connection and send http request using curl

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -9,7 +9,7 @@ server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
 server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
-server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+server.config["MYSQL_PORT"] = int(os.environ.get("MYSQL_PORT"))
 
 @server.route("/login", methods = ["POST"])
 def login():
@@ -22,7 +22,7 @@ def login():
     # cursor is used to execute sql queries.
     res = cur.execute(
          "SELECT email, password FROM user WHERE email=%s",
-         (auth.username)
+         (auth.username,)
     )
     if res > 0:
         user_row = cur.fetchone()
